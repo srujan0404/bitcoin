6ae10c7ee1ff9c9610129e7159c16db972cad14e
srujan0404 <srujan.23bcs10107@ms.sst.scaler.com>
2024-07-07 01:02:07 +0530

    changes

--- functions.js
+++ functions.js
@@ -4,6 +4,8 @@ const EC = require("elliptic").ec;
 const ecdsa = new EC("secp256k1");
 const { createHash } = require("crypto");
 const { verify } = require("crypto");
+const { sha256 } = require("./hashes");
+const { parseDER, verifyECDSASignature } = require("./EcdsaVerification");
 
 function toHex(value) {
   return value
@@ -14,13 +16,6 @@ function toHex(value) {
         : value.toString(16).length + 1,
       "0"
     );
-
-    concatStr +=
-      littleEndian(vin.txid) +
-      littleEndian(toHex(vin.vout).padStart(8, "0")) +
-      sigSize.toString(16).padStart(2, "0") +
-      vin.scriptsig +
-      littleEndian(vin.sequence.toString(16));
 }
 
 function littleEndian(data) {
@@ -28,38 +23,8 @@ function littleEndian(data) {
   for (let i = 0; i < data.length; i += 2) {
     pairs.unshift(data.substring(i, i + 2));
   }
-  concatStr +=
-    littleEndian(vout.value.toString(16).padStart(16, "0")) +
-    toHex(vout.scriptpubkey.length / 2)
-      .toString(16)
-      .padStart(2, "0") +
-    vout.scriptpubkey;
   return pairs.join("");
 }
-
-function create_wtxid(tx) {
-  return doubleHash(
-    littleEndian(tx.version.toString(16).padStart(8, "0")) +
-      "00" + //marker
-      "01" + //flag
-      tx.vin.length.toString(16).padStart(2, "0") + //number of inputs
-      concVin(tx) + //inputs
-      tx.vout.length.toString(16).padStart(2, "0") + //number of outputs
-      concVout(tx) + //outputs
-      conc_witness(tx) + //witness
-      littleEndian(tx.locktime.toString(16).padStart(8, "0"))
-  ); //locktime
-}
-
- for (const vinEntry of tx.vin) {
-      const witness = vinEntry.witness;
-      if (witness) { // Check if witness is defined
-          concstr += (witness.length).toString(16).padStart(2, '0');
-          for (const wit of witness) {
-              concstr += (wit.length / 2).toString(16).padStart(2, '0') + wit;
-          }
-        }
-  }
 
 function ripemd160(data) {
   const hash = crypto.createHash("ripemd160");
@@ -74,7 +39,7 @@ function doubleHash(dataHex) {
     .digest("hex");
   const secondHash = crypto
     .createHash("sha256")
-    .update(Buffer.from(firstHash, "hex"))
+    .update(firstHash, "hex")
     .digest("hex");
   return secondHash;
 }
@@ -86,9 +51,9 @@ function concVin(transaction) {
     concatStr +=
       littleEndian(vin.txid) +
       littleEndian(toHex(vin.vout).padStart(8, "0")) +
-      toHex(sigSize).padStart(2, "0") +
+      sigSize.toString(16).padStart(2, "0") +
       vin.scriptsig +
-      littleEndian(toHex(vin.sequence));
+      littleEndian(vin.sequence.toString(16));
   });
   return concatStr;
 }
@@ -98,27 +63,28 @@ function concVout(transaction) {
   transaction.vout.forEach((vout) => {
     concatStr +=
       littleEndian(vout.value.toString(16).padStart(16, "0")) +
-      toHex(vout.scriptpubkey.length / 2).padStart(2, "0") +
+      toHex(vout.scriptpubkey.length / 2)
+        .toString(16)
+        .padStart(2, "0") +
       vout.scriptpubkey;
   });
   return concatStr;
 }
 
-function serializeP2pkh(transaction) {
-  const serializeP2pkh =
+function serializeTransaction(transaction) {
+  const serializeTransaction =
     littleEndian(transaction.version.toString(16).padStart(8, "0")) +
     toHex(transaction.vin.length).padStart(2, "0") +
     concVin(transaction) +
     transaction.vout.length.toString(16).padStart(2, "0") +
     concVout(transaction) +
     littleEndian(toHex(transaction.locktime)).padEnd(8, "0");
-  return serializeP2pkh;
+  return doubleHash(serializeTransaction);
 }
 
 function verifyFiles(data) {
-  const data1 = serializeP2pkh(data);
-  const data2 = littleEndian(doubleHash(data1));
-  const dataBytes = Buffer.from(data2, "hex");
+  const data1 = littleEndian(serializeTransaction(data));
+  const dataBytes = Buffer.from(data1, "hex");
   return crypto.createHash("sha256").update(dataBytes).digest("hex") + ".json";
 }
 
@@ -158,52 +124,6 @@ function createDigest(transaction, index) {
   return doubleHash(concatStr);
 }
 
-function parseDER(serialized) {
-  // Extract the length of the R element
-  const rLength = parseInt(serialized.substring(6, 8), 16) * 2;
-  // Calculate the start and end positions of R
-  const rStart = 8;
-  const rEnd = rStart + rLength;
-  // Extract R
-  const r = serialized.substring(rStart, rEnd);
-
-  // Extract the length of the S element
-  const sLength = parseInt(serialized.substring(rEnd + 2, rEnd + 4), 16) * 2;
-  // Calculate the start and end positions of S
-  const sStart = rEnd + 4;
-  const sEnd = sStart + sLength;
-  // Extract S
-  const s = serialized.substring(sStart, sEnd);
-  return { r, s };
-}
-
-function parseDER(serialized) {
-    // Extract the length of the R element
-    const rLength = parseInt(serialized.substring(6, 8), 16) * 2;
-    // Calculate the start and end positions of R
-    const rStart = 8;
-    const rEnd = rStart + rLength;
-    // Extract R
-    const r = serialized.substring(rStart, rEnd);
-
-    // Extract the length of the S element
-    const sLength = parseInt(serialized.substring(rEnd + 2, rEnd + 4), 16) * 2;
-    // Calculate the start and end positions of S
-    const sStart = rEnd + 4;
-    const sEnd = sStart + sLength;
-    // Extract S
-    const s = serialized.substring(sStart, sEnd);
-    return { r, s };
-  }
-
-  function verifyECDSASignature(publicKeyHex, signatureHex, messageHex) {
-    const ecdsa = new EC("secp256k1");
-    const key = ecdsa.keyFromPublic(publicKeyHex, "hex");
-    const signature = parseDER(signatureHex);
-    const isValid = key.verify(messageHex, signature);
-    return isValid;
-  }
-
 function checkSigP2PKH(transaction, i) {
   const message = createDigest(transaction, i);
   const isValid = verifyECDSASignature(
@@ -222,9 +142,9 @@ function checkStack(transaction) {
   for (const vin_entry of vin) {
     const stack = [];
     const vin_asm = vin_entry.scriptsig_asm.split(" ");
-    // console.log(vin_asm);
+
     stack.push(vin_asm[1]);
-    // console.log(stack);
+
     stack.push(vin_asm[3]);
     const script = vin_entry.prevout.scriptpubkey_asm.split(" ");
     for (let i = 0; i < script.length; i++) {
@@ -278,7 +198,7 @@ module.exports = {
   parseDER,
   checkSigP2PKH,
   createVinDigest,
-  serializeP2pkh,
+  serializeTransaction,
   concVout,
   concVin,
   doubleHash,
@@ -287,179 +207,4 @@ module.exports = {
   toHex,
 };
 
-tx = {
-  txid: "d63e67e47999283ee784f959edef4ff80b507b06b03623030d7c3042e709047b",
-  version: 2,
-  locktime: 0,
-  vin: [
-    {
-      txid: "32edfb6330b27344f88655887d3ef2258a1281b6a9cc95ee02ff4f2188e91d5d",
-      vout: 0,
-      prevout: {
-        scriptpubkey: "76a9147e00ab284ace44f6cf1274957f2b743b233a2f6688ac",
-        scriptpubkey_asm:
-          "OP_DUP OP_HASH160 OP_PUSHBYTES_20 7e00ab284ace44f6cf1274957f2b743b233a2f66 OP_EQUALVERIFY OP_CHECKSIG",
-        scriptpubkey_type: "p2pkh",
-        scriptpubkey_address: "1CVEwdEGQHrwxu17Mbq5U1d9y34VHBscXE",
-        value: 29390611,
-      },
-      scriptsig:
-        "4830450221008d6c25a55640e5e6a86e9115e02b2549f95eb8f980435afbc0ab5686abe0a6430220163d6d208d87890d63d90af8136cd8fc4461d5354bf2a950a11e47d2874d355c012103442520eb769b97b1f4d0f727a795351150a449a08f69079fb7363f4880bd6f63",
-      scriptsig_asm:
-        "OP_PUSHBYTES_72 30450221008d6c25a55640e5e6a86e9115e02b2549f95eb8f980435afbc0ab5686abe0a6430220163d6d208d87890d63d90af8136cd8fc4461d5354bf2a950a11e47d2874d355c01 OP_PUSHBYTES_33 03442520eb769b97b1f4d0f727a795351150a449a08f69079fb7363f4880bd6f63",
-      is_coinbase: false,
-      sequence: 4294967293,
-    },
-    {
-      txid: "48aa0c5aae0065e12c256663f408626faeb99602a139cbdd961c85d1e2b3d7a4",
-      vout: 0,
-      prevout: {
-        scriptpubkey: "76a9147e00ab284ace44f6cf1274957f2b743b233a2f6688ac",
-        scriptpubkey_asm:
-          "OP_DUP OP_HASH160 OP_PUSHBYTES_20 7e00ab284ace44f6cf1274957f2b743b233a2f66 OP_EQUALVERIFY OP_CHECKSIG",
-        scriptpubkey_type: "p2pkh",
-        scriptpubkey_address: "1CVEwdEGQHrwxu17Mbq5U1d9y34VHBscXE",
-        value: 9037968,
-      },
-      scriptsig:
-        "483045022100ea8f9b4c918c283bcc16ca1c99d33409e25f50d37fff8f321e95d5cf8516a04e022006e16fab2df84bf279aa09bc7ee843a836c8259003973551d99c477f1abde253012103442520eb769b97b1f4d0f727a795351150a449a08f69079fb7363f4880bd6f63",
-      scriptsig_asm:
-        "OP_PUSHBYTES_72 3045022100ea8f9b4c918c283bcc16ca1c99d33409e25f50d37fff8f321e95d5cf8516a04e022006e16fab2df84bf279aa09bc7ee843a836c8259003973551d99c477f1abde25301 OP_PUSHBYTES_33 03442520eb769b97b1f4d0f727a795351150a449a08f69079fb7363f4880bd6f63",
-      is_coinbase: false,
-      sequence: 4294967293,
-    },
-    {
-      txid: "f3dbebf377c2ddaba0bdc9731c0cafa5e6057534cb1885493c7499cfdce11972",
-      vout: 1,
-      prevout: {
-        scriptpubkey: "001457f5cb1cb375d7e487c9d02f4d581df5c5d7a51f",
-        scriptpubkey_asm:
-          "OP_0 OP_PUSHBYTES_20 57f5cb1cb375d7e487c9d02f4d581df5c5d7a51f",
-        scriptpubkey_type: "v0_p2wpkh",
-        scriptpubkey_address: "bc1q2l6uk89nwht7fp7f6qh56kqa7hza0fglh9pze6",
-        value: 13580,
-      },
-      scriptsig: "",
-      scriptsig_asm: "",
-      witness: [
-        "3045022100c1015c8af17423a0e40d8159156d992eb253f12c4fd64e4f9db78621489de1bc022066007488c824a0ecfd62cd593f66a726e1e1d7b55f898328a49fabf3557fc03801",
-        "03f00d8ca2c0ba3b5a594465cdff97973c893777932ede0cde1ffa98f06868acec",
-      ],
-      is_coinbase: false,
-      sequence: 4294967293,
-    },
-    {
-      txid: "f7e70896b5c4c510b58f7d679c7df69953ffb4eb49e8f68c5ebab4440b763a81",
-      vout: 0,
-      prevout: {
-        scriptpubkey: "76a9147e00ab284ace44f6cf1274957f2b743b233a2f6688ac",
-        scriptpubkey_asm:
-          "OP_DUP OP_HASH160 OP_PUSHBYTES_20 7e00ab284ace44f6cf1274957f2b743b233a2f66 OP_EQUALVERIFY OP_CHECKSIG",
-        scriptpubkey_type: "p2pkh",
-        scriptpubkey_address: "1CVEwdEGQHrwxu17Mbq5U1d9y34VHBscXE",
-        value: 700549,
-      },
-      scriptsig:
-        "4830450221008fa5e43eee0c74dcaf25f3b110951617a023d5601e4b2c20174addc2ec703b4602207755d08a00449cbf208bd8c84235d0188a524778d6e20bbd3ae0c6676f4be2c6012103442520eb769b97b1f4d0f727a795351150a449a08f69079fb7363f4880bd6f63",
-      scriptsig_asm:
-        "OP_PUSHBYTES_72 30450221008fa5e43eee0c74dcaf25f3b110951617a023d5601e4b2c20174addc2ec703b4602207755d08a00449cbf208bd8c84235d0188a524778d6e20bbd3ae0c6676f4be2c601 OP_PUSHBYTES_33 03442520eb769b97b1f4d0f727a795351150a449a08f69079fb7363f4880bd6f63",
-      is_coinbase: false,
-      sequence: 4294967293,
-    },
-    {
-      txid: "242d82904b8e7def2d0a37a55da6aeaa9d372ab18784381acb53ee364d0b35bb",
-      vout: 0,
-      prevout: {
-        scriptpubkey: "76a9147e00ab284ace44f6cf1274957f2b743b233a2f6688ac",
-        scriptpubkey_asm:
-          "OP_DUP OP_HASH160 OP_PUSHBYTES_20 7e00ab284ace44f6cf1274957f2b743b233a2f66 OP_EQUALVERIFY OP_CHECKSIG",
-        scriptpubkey_type: "p2pkh",
-        scriptpubkey_address: "1CVEwdEGQHrwxu17Mbq5U1d9y34VHBscXE",
-        value: 547424,
-      },
-      scriptsig:
-        "4730440220465fa2f5bf3b440f8140d5d2ea2161bf7201bbac1e78271dd8bb8a52f170c179022059a8223a0c49500c4cd5d61a2cf3c164e330a979529aa1aa2231074df7d9fcf6012103442520eb769b97b1f4d0f727a795351150a449a08f69079fb7363f4880bd6f63",
-      scriptsig_asm:
-        "OP_PUSHBYTES_71 30440220465fa2f5bf3b440f8140d5d2ea2161bf7201bbac1e78271dd8bb8a52f170c179022059a8223a0c49500c4cd5d61a2cf3c164e330a979529aa1aa2231074df7d9fcf601 OP_PUSHBYTES_33 03442520eb769b97b1f4d0f727a795351150a449a08f69079fb7363f4880bd6f63",
-      is_coinbase: false,
-      sequence: 4294967293,
-    },
-    {
-      txid: "6afebd807c8984e50b1458b0aa3f4371f03f6f8f6bb7309a8b248a6115cd9c3d",
-      vout: 0,
-      prevout: {
-        scriptpubkey: "76a9147e00ab284ace44f6cf1274957f2b743b233a2f6688ac",
-        scriptpubkey_asm:
-          "OP_DUP OP_HASH160 OP_PUSHBYTES_20 7e00ab284ace44f6cf1274957f2b743b233a2f66 OP_EQUALVERIFY OP_CHECKSIG",
-        scriptpubkey_type: "p2pkh",
-        scriptpubkey_address: "1CVEwdEGQHrwxu17Mbq5U1d9y34VHBscXE",
-        value: 1287906,
-      },
-      scriptsig:
-        "4830450221009c10ef351650eacfa9d4a933a4657dfdd0c65a5092695d3ce2f310614f7d1bd002204a8f0fd76d9befb79767041fa0123e41906d858bf3008b653349517e7b7e270d012103442520eb769b97b1f4d0f727a795351150a449a08f69079fb7363f4880bd6f63",
-      scriptsig_asm:
-        "OP_PUSHBYTES_72 30450221009c10ef351650eacfa9d4a933a4657dfdd0c65a5092695d3ce2f310614f7d1bd002204a8f0fd76d9befb79767041fa0123e41906d858bf3008b653349517e7b7e270d01 OP_PUSHBYTES_33 03442520eb769b97b1f4d0f727a795351150a449a08f69079fb7363f4880bd6f63",
-      is_coinbase: false,
-      sequence: 4294967293,
-    },
-    {
-      txid: "4df31ded7b4a7c28cd182d1e9b362a6a7e4fad4ac8231fec4d93785960173544",
-      vout: 1,
-      prevout: {
-        scriptpubkey: "001457f5cb1cb375d7e487c9d02f4d581df5c5d7a51f",
-        scriptpubkey_asm:
-          "OP_0 OP_PUSHBYTES_20 57f5cb1cb375d7e487c9d02f4d581df5c5d7a51f",
-        scriptpubkey_type: "v0_p2wpkh",
-        scriptpubkey_address: "bc1q2l6uk89nwht7fp7f6qh56kqa7hza0fglh9pze6",
-        value: 3129839,
-      },
-      scriptsig: "",
-      scriptsig_asm: "",
-      witness: [
-        "304402206160c6fa6b90558ae18ffd540ed908148b31a3c594152e3c1c96b0f6be1ef841022050fea29bd6e0c7f9254fd40ef7a7132b32db36e0e12b851867db6848b4e2868001",
-        "03f00d8ca2c0ba3b5a594465cdff97973c893777932ede0cde1ffa98f06868acec",
-      ],
-      is_coinbase: false,
-      sequence: 4294967293,
-    },
-  ],
-  vout: [
-    {
-      scriptpubkey: "0014c9ca81f8508e870b8baecc4ac4d2fed294a4d921",
-      scriptpubkey_asm:
-        "OP_0 OP_PUSHBYTES_20 c9ca81f8508e870b8baecc4ac4d2fed294a4d921",
-      scriptpubkey_type: "v0_p2wpkh",
-      scriptpubkey_address: "bc1qe89gr7zs36rshzawe39vf5h76222fkfpxvsywe",
-      value: 44094176,
-    },
-  ],
-};
-
-console.log(littleEndian(serializeP2pkh(tx)));
-
-function parseDER(serialized) {
-  // Extract the length of the R element
-  const rLength = parseInt(serialized.substring(6, 8), 16) * 2;
-  // Calculate the start and end positions of R
-  const rStart = 8;
-  const rEnd = rStart + rLength;
-  // Extract R
-  const r = serialized.substring(rStart, rEnd);
-
-  // Extract the length of the S element
-  const sLength = parseInt(serialized.substring(rEnd + 2, rEnd + 4), 16) * 2;
-  // Calculate the start and end positions of S
-  const sStart = rEnd + 4;
-  const sEnd = sStart + sLength;
-  // Extract S
-  const s = serialized.substring(sStart, sEnd);
-  return { r, s };
-}
-
-function verifyECDSASignature(publicKeyHex, signatureHex, messageHex) {
-  const ecdsa = new EC("secp256k1");
-  const key = ecdsa.keyFromPublic(publicKeyHex, "hex");
-  const signature = parseDER(signatureHex);
-  const isValid = key.verify(messageHex, signature);
-  return isValid;
-}
+// console.log(littleEndian(serializeTransaction(tx)));
--- mine.js
+++ mine.js
@@ -1,44 +1,43 @@
 const { block_header } = require("./block.js");
 const { doubleHash } = require("./hashes.js");
 const { littleEndian } = require("./functions.js");
-const { txns, coinbase_tx } = require("./coinbasetxn.js");
+const { txAll, coinbase_tx } = require("./coinbasetxn.js");
 
 // Implement the mine function that takes a block header as input and returns the mined block
 function mine(block_header) {
-  let nonce = 0;
   const target = Buffer.from(
-    "0000ffff00000000000000000000000000000000000000000000000000000000"
+    "0000ffff00000000000000000000000000000000000000000000000000000000",
+    "hex"
   ); // Convert target to buffer
+  let nonce = 0;
   let paddedNonce = nonce.toString(16).padStart(8, "0"); // Pad nonce with leading zeros to make it 4 bytes
-
   while (true) {
     const hash = Buffer.from(
-      littleEndian(doubleHash(block_header + littleEndian(paddedNonce))),
+      littleEndian(doubleHash(block_header + paddedNonce)),
       "hex"
     ); // Convert hash to buffer
-    if (hash.compare(target) < 0) {
-      // Compare hashes using buffer compare method
-      return block_header + littleEndian(paddedNonce);
+    // Compare hashes using buffer compare method
+    if (target.compare(hash) > 0) {
+      console.log(paddedNonce);
+      return block_header + paddedNonce;
     }
+
     nonce++;
     paddedNonce = nonce.toString(16).padStart(8, "0"); // Update nonce with leading zeros
   }
 }
 
 let mined_block = mine(block_header);
-// console.log(mined_block);
 
 const fs = require("fs");
 function writeToOutputFile() {
   const outputData = [];
   outputData.push(mined_block);
-  outputData.push(...txns);
-
-  // Join data with newline character
-  const outputContent = outputData.join("\n");
   outputData.push(coinbase_tx);
+  outputData.push(...txAll);
 
-  // Write data to output.txt file
+  const outputContent = outputData.join("\n");
+
   fs.writeFile("output.txt", outputContent, (err) => {
     if (err) {
       console.error("Error writing to file:", err);
@@ -48,7 +47,4 @@ function writeToOutputFile() {
   });
 }
 
-// Call function to write data to file
 writeToOutputFile();
-
-console.log(mined_block.toString("hex").length);
